3bad31ce4b0ade0737a80e14a2d6b47e575b1c12
dpstrange <drew@strange.onl>
2020-01-06 11:09:19 -0600

    Optimize

--- js/pixelate.js
+++ js/pixelate.js
@@ -10,46 +10,36 @@
     $.fn.pixelate = function (options) {
         var defaults = {
             focus: 0.5,
-            canvasID: "pixelateCanvas",
+            canvasId: "pixelate-canvas"
         }, settings = $.extend({}, defaults, options);
-
-        this.each(function (index) {
-            var width = this.naturalWidth;
-            var height = this.naturalHeight;
-
-            var idSelector = "#" + settings.canvasID + index;
+        
+        return this.each(function(index) {
+            var id = settings.canvasId + "-" + index,
+                width = this.width,
+                height = this.height;
 
             $(this).hide();
-  
-            $(this).parent().children("canvas").remove();
-  
-            $(this).after("<canvas id='" + idSelector.slice(1) + "' class='pixelate'>");
-  
-            $(idSelector).get(0).width = width;
-            $(idSelector).get(0).height = height;
+            $(this).after("<canvas id='" + id + "' height='" + height +"' width='" + width +"'>");
 
-            var context = $(idSelector).get(0).getContext("2d");
-    
+            var context = $("#" + id).get(0).getContext("2d");
+
             context.imageSmoothingEnabled = false;
-  
+
             function pixelate(image) {
-                var element = $("#" + settings.canvasID + index).get(0);
-        
-                var relativeWidth = element.width;
-                var relativeHeight = element.height;
+                var element = $("#" + id).get(0),
+                    relativeWidth = width,
+                    relativeHeight = height;
 
                 if (settings.focus != 1) {
                     relativeWidth = relativeWidth * settings.focus * 0.25;
                     relativeHeight = relativeHeight * settings.focus * 0.25;
                 }
-        
+
                 context.drawImage(image, 0, 0, relativeWidth + 1, relativeHeight + 1);
-                context.drawImage(element, 0, 0, relativeWidth, relativeHeight, 0, 0, element.width, element.height);
-            }
+                context.drawImage(element, 0, 0, relativeWidth, relativeHeight, 0, 0, width, height);
+            };
 
             pixelate(this);
         });
-
-        return this;
     };
 }(jQuery));
